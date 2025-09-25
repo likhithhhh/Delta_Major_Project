f6adffb591db84097be2d667dfbc8166a83e2f0b
Likhith <likhithchappati@gmail.com>
2025-09-25 14:39:04 +0530

    modified app.js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));``
 app.use(express.urlencoded({extended:true}));
 
-app.use("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send("response success");
 });
 
